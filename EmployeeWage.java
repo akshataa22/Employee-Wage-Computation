9577d09f9dd4b529cbc02d9198f341a7189944de
akshataa22 <dhanawadeakshata53@gmail.com>
2023-11-29 23:00:21 +0530

    Part Time Employee wage Check

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,18 +8,30 @@ public class EmployeeWage {
 
         final int wagePerHour = 20;
         final int fullDayHour = 8;
+        final int partTimeHour = 4;
         final int workingDays = 20;
         int totalWage = 0;
         for (int day = 0; day < workingDays; day++) {
-            int empCheck = new Random().nextInt(2); // using random function to generate 0 and 1
-            if (empCheck == 1) {
-                System.out.println("Day " + (day + 1) + ": Employee is Present.");
-                int dailyWage = wagePerHour * fullDayHour;
-                totalWage += dailyWage;
-            } else {
-                System.out.println("Day " + (day + 1) + ": Employee is absent.");
-            }
+            int empCheck = new Random().nextInt(3); // using random function to generate 0, 1, or 2
+
+            int dailyWage = 0;
+        switch (empCheck) {
+            case 0:
+                System.out.println("Day " + (day + 1) + ": Employee is Absent.");
+                break;
+            case 1:
+                System.out.println("Day " + (day + 1) + ": Employee is Present (Full Time).");
+                dailyWage = wagePerHour * fullDayHour;
+                break;
+            case 2:
+                System.out.println("Day " + (day + 1) + ": Employee is Present (Part Time).");
+                dailyWage = wagePerHour * partTimeHour;
+                break;
         }
+
+        totalWage += dailyWage;
+        System.out.println("Daily Wage: " + dailyWage);
+    }
         System.out.println("Total Monthly Wage: " + totalWage);
     }
 }
