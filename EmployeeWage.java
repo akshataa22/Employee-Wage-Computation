87e843037705ed70069e2dba815beb02ebb29751
akshataa22 <dhanawadeakshata53@gmail.com>
2023-11-30 01:02:21 +0530

    Calculating Wages till a condition of total working hours or days is reached for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -9,29 +9,39 @@ public class EmployeeWage {
         final int wagePerHour = 20;
         final int fullDayHour = 8;
         final int partTimeHour = 4;
-        final int workingDays = 20;
+        final int maxWorkingDays = 20;
+        final int maxWorkingHours = 100;
+
         int totalWage = 0;
-        for (int day = 0; day < workingDays; day++) {
+        int totalHoursWorked = 0;
+        int totalDaysWorked = 0;
+        while (totalDaysWorked < maxWorkingDays && totalHoursWorked < maxWorkingHours) {
             int empCheck = new Random().nextInt(3); // using random function to generate 0, 1, or 2
 
             int dailyWage = 0;
+            int dailyHoursWorked = 0;
         switch (empCheck) {
             case 0:
-                System.out.println("Day " + (day + 1) + ": Employee is Absent.");
+                System.out.println("Day " + (totalDaysWorked + 1) + ": Employee is Absent.");
                 break;
             case 1:
-                System.out.println("Day " + (day + 1) + ": Employee is Present Full Day.");
-                dailyWage = wagePerHour * fullDayHour;
+                System.out.println("Day " + (totalDaysWorked + 1) + ": Employee is Present Full Day.");
+                dailyHoursWorked = fullDayHour;
                 break;
             case 2:
-                System.out.println("Day " + (day + 1) + ": Employee is Present Half Day.");
-                dailyWage = wagePerHour * partTimeHour;
+                System.out.println("Day " + (totalDaysWorked + 1) + ": Employee is Present Half Day.");
+                dailyHoursWorked = partTimeHour;
                 break;
         }
 
+        totalDaysWorked++;
+        totalHoursWorked += dailyHoursWorked;
+        dailyWage = wagePerHour * dailyHoursWorked;
         totalWage += dailyWage;
         System.out.println("Daily Wage: " + dailyWage);
     }
         System.out.println("Total Wages for a month: " + totalWage);
+        System.out.println("Total Hours Worked: " + totalHoursWorked);
+        System.out.println("Total Days Worked: " + totalDaysWorked);
     }
 }
